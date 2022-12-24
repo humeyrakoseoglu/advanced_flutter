8985ddebd2f4390c029242f032b00f4687587e8d
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2022-12-24 17:46:37 +0300

    Presentation Layer- OnBoarding Screen

--- lib/presentation/onBoarding/onBoarding.dart
+++ lib/presentation/onBoarding/onBoarding.dart
@@ -1,6 +1,11 @@
+import 'package:advanced_flutter/presentation/resources/assets_manager.dart';
 import 'package:advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter/presentation/resources/string_manager.dart';
+import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -10,11 +15,170 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  //page controller oluştur
+  PageController _pageController = PageController(initialPage: 0);
+  //verileri içeren liste yaz
+  late final List<SliderObject> _list = _getSliderData();
+  //mevcut indexi tutalım listedeki nesneler için
+  int _currentIndex = 0;
+  List<SliderObject> _getSliderData() => [
+    SliderObject(AppStrings.onBoardingTitle1, AppStrings.onBoardingSubTitle1, ImageAssets.onBoardingLogo1),
+    SliderObject(AppStrings.onBoardingTitle2, AppStrings.onBoardingSubTitle2, ImageAssets.onBoardingLogo2),
+    SliderObject(AppStrings.onBoardingTitle3, AppStrings.onBoardingSubTitle3, ImageAssets.onBoardingLogo3),
+    SliderObject(AppStrings.onBoardingTitle4, AppStrings.onBoardingSubTitle4, ImageAssets.onBoardingLogo4),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(child: Text("Welcome to on boarding"),),
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged:(index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        itemBuilder: (context, index) {
+        return OnBoardingPage(_list[index]);
+        //return OnBoardingPage
+        }),
+    //sağ alttaki skip butonu için
+    bottomSheet: Container(
+      color:ColorManager.white,
+      height: AppSize.s100,
+      child: Column(
+        children: [
+          Align(alignment: Alignment.centerRight,
+          child:TextButton(onPressed: () {}, child: Text(
+              AppStrings.skip,
+            style:Theme.of(context).textTheme.subtitle2,textAlign: TextAlign.end,
+          ), ),),
+          //add layout for indicator and arrows
+          _getBottomSheetWidget(),
+        ]
+      ),
+
+    ) ,);
+    }
+
+
+//Bottom için ayrı bir widget oluşturalım
+Widget _getBottomSheetWidget(){
+  return Container(
+    color: ColorManager.primary,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        //left arrow
+        Padding(padding: EdgeInsets.all(AppPadding.p14),
+        child: GestureDetector(
+          child: SizedBox(
+            height: AppSize.s20,
+            width: AppSize.s20,
+            child: SvgPicture.asset(ImageAssets.leftArrowIc),
+          ),
+          onTap: (){
+            //go previous slide. Animates the controlled PageView from the current page to the given page.
+            _pageController.animateToPage(_getPreviousIndex(), duration: Duration(milliseconds: DurationConstant.d300), curve: Curves.bounceInOut);
+          },
+        ),),
+        //circles indicator
+        Row(
+          children: [
+            //indexe göre slide'ın olduğu circle'ı belirliyoruz
+            for(int i = 0; i< _list.length;i++)
+              Padding(padding: EdgeInsets.all(AppPadding.p8),
+              child: _getProperCircle(i),)
+          ],
+        ),
+        //right arrow
+        Padding(padding: EdgeInsets.all(AppPadding.p14),
+          child: GestureDetector(
+            child: SizedBox(
+              height: AppSize.s20,
+              width: AppSize.s20,
+              child: SvgPicture.asset(ImageAssets.rightarrowIc),
+            ),
+            onTap: (){
+              //go next slide. Animates the controlled PageView from the current page to the given page.
+              _pageController.animateToPage(_getNextIndex(), duration: Duration(milliseconds: DurationConstant.d300), curve: Curves.bounceInOut);
+            },
+          ),),
+      ],
+    ),
+  );
+}
+
+  int _getPreviousIndex(){
+    int previousIndex = _currentIndex--; //-1
+    if(previousIndex == -1) {
+      _currentIndex = _list.length -1; // infinite loop to go to the length of slider list
+    }
+    return _currentIndex;
+  }
+
+  int _getNextIndex(){
+    int nextIndex = _currentIndex++; //+1
+    if(nextIndex >= _list.length) {
+      _currentIndex = 0; // infinite loop to go to first item inside the slider
+    }
+    return _currentIndex;
+  }
+
+  Widget _getProperCircle(int index){
+    if(index == _currentIndex){
+      return SvgPicture.asset(ImageAssets.hollowCircleIc); // selected slider
+    }else{
+      return SvgPicture.asset(ImageAssets.solidCircleIc); // unselected slider
+    }
+}
+}
+
+class OnBoardingPage extends StatelessWidget{
+  SliderObject _sliderObject;
+
+  OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(height: AppSize.s40,),
+        Padding(padding: const EdgeInsets.all(AppPadding.p8),
+        child: Text(
+          _sliderObject.title,style: Theme.of(context).textTheme.headline1,textAlign: TextAlign.center
+        ),),
+        Padding(padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+              _sliderObject.subTitle,style: Theme.of(context).textTheme.subtitle1,textAlign: TextAlign.center
+          ),),
+        SizedBox(
+          height: AppSize.s60,
+        ),
+        //Image Widget
+        SvgPicture.asset(_sliderObject.image),
+      ],
     );
   }
+
+}
+
+class SliderObject{
+  late String title;
+  late String subTitle;
+  late String image;
+  SliderObject(this.title, this.subTitle, this.image);
+
+
 }
